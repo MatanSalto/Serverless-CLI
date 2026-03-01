bcb3d18fb484d0137cc8fd8188c23966a474584a
EC2 Default User <ec2-user@ip-172-31-7-41.eu-north-1.compute.internal>
2026-03-01 10:06:17 +0000

    feat: added root command for the CLI

---
+++ cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	kubeconfig string
+	namespace  string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "serverless-cli",
+	Short: "Run Python workloads serverlessly on Kubernetes",
+	Long: `A CLI that runs your Python code on your Kubernetes cluster
+without managing infrastructure.`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&kubeconfig, "kubeconfig", "", "Path to kubeconfig file (if not provided, the default is $KUBECONFIG)")
+	rootCmd.PersistentFlags().StringVar(&namespace, "namespace", "serverless-python", "The Kubernetes namespace to run workloads in")
+}
