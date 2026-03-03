e5e67731cc945d1c6026ab192b3752e0fd339d4a
EC2 Default User <ec2-user@ip-172-31-1-90.il-central-1.compute.internal>
2026-03-03 08:37:14 +0000

    feat: added option to configure volume and volumeMounts for the source code in the kubernetes job

--- pkg/kube/job.go
+++ pkg/kube/job.go
@@ -12,11 +12,24 @@ import (
 
 // Job represents a Kubernetes Job.
 type JobParams struct {
+	// Job name
 	Name      string
+	// Kubernetes namespace
 	Namespace string
+	// Container image
 	Image     string
+	// Command to run in the container
 	Command   []string
+	// Arguments to pass to the command
 	Args      []string
+	// Environment variables to set in the container
+	Env []corev1.EnvVar
+	// Name of the ConfigMap to mount as a volume
+	ConfigMapName string
+	// Path in the container at which to mount the ConfigMap
+	MountPath string
+	// Items to mount from the ConfigMap
+	ConfigMapItems []corev1.KeyToPath
 }
 
 // CreateJob creates and submits a Job to the cluster.
@@ -31,6 +44,42 @@ func CreateJob(ctx context.Context, client kubernetes.Interface, jobParams JobPa
 		return nil, errors.New("image is required")
 	}
 
+	container := corev1.Container{
+		Name:    jobParams.Name,
+		Image:   jobParams.Image,
+		Command: jobParams.Command,
+		Args:    jobParams.Args,
+		Env:     jobParams.Env,
+	}
+
+	podSpec := corev1.PodSpec{
+		RestartPolicy: corev1.RestartPolicyNever,
+		Containers:   []corev1.Container{container},
+	}
+
+	if jobParams.ConfigMapName != "" {
+		volName := "source-code"
+		podSpec.Volumes = []corev1.Volume{
+			{
+				Name: volName,
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						LocalObjectReference: corev1.LocalObjectReference{Name: jobParams.ConfigMapName},
+						Items:                jobParams.ConfigMapItems,
+					},
+				},
+			},
+		}
+		mountPath := jobParams.MountPath
+		if mountPath == "" {
+			mountPath = "/opt/code"
+		}
+		container.VolumeMounts = []corev1.VolumeMount{
+			{Name: volName, MountPath: mountPath},
+		}
+		podSpec.Containers[0] = container
+	}
+
 	job := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      jobParams.Name,
@@ -38,17 +87,7 @@ func CreateJob(ctx context.Context, client kubernetes.Interface, jobParams JobPa
 		},
 		Spec: batchv1.JobSpec{
 			Template: corev1.PodTemplateSpec{
-				Spec: corev1.PodSpec{
-					RestartPolicy: corev1.RestartPolicyNever,
-					Containers: []corev1.Container{
-						{
-							Name:    jobParams.Name,
-							Image:   jobParams.Image,
-							Command: jobParams.Command,
-							Args:    jobParams.Args,
-						},
-					},
-				},
+				Spec: podSpec,
 			},
 		},
 	}
