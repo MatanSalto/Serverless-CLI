6bec4ca1feecfda71c65a31c058a78b6966a72b5
EC2 Default User <ec2-user@ip-172-31-1-90.il-central-1.compute.internal>
2026-03-11 13:55:40 +0000

    refactor: created dedicated function to return the REST config so it could be used in port-forwarding

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -1,31 +1,38 @@
 package kube
 
 import (
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 	"os"
 	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // NewClientSet returns a clientSet for the cluster.
 func NewClientSet() (kubernetes.Interface, error) {
-	// Use KUBECONFIG env if set, otherwise use ~/.kube/config
+	config, err := restConfig()
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
+}
+
+// NewRestConfig returns the rest.Config for the cluster (e.g. for port-forwarding).
+func NewRestConfig() (*rest.Config, error) {
+	return restConfig()
+}
+
+func restConfig() (*rest.Config, error) {
 	var kubeconfig string
 	if os.Getenv("KUBECONFIG") != "" {
 		kubeconfig = os.Getenv("KUBECONFIG")
 	} else {
 		kubeconfig = filepath.Join(os.Getenv("HOME"), ".kube", "config")
 	}
-
-	// Build config from flags or use default config
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		return nil, err
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-	return clientset, nil
+	return clientcmd.BuildConfigFromFlags("", kubeconfig)
 }
